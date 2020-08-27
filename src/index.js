1ddb7048207cf0208db9f3cf520d580852e8d50a
Daegudude <k3hppk@gmail.com>
2020-08-27 12:24:13 +0900

    Tab design done

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ let navBar = content.querySelector('ul');
 // Add eventListeners to all li
 for(let i = 0; i < navBar.children.length; i++) {
   let li = navBar.children[i];
+
+  
   li.addEventListener('click', (event) => {
     let tab = li.innerHTML;
 
@@ -32,26 +34,47 @@ for(let i = 0; i < navBar.children.length; i++) {
       case 'Home':
         main.innerHTML = '';
         main.appendChild(initialLoad());
+
+        // Make a bottom line
+        navRemoveBottomLine();
+        li.style.borderBottom = "5px solid #20de83";        
         break;
 
       case 'About':
         main.innerHTML = '';
         main.appendChild(loadAbout());
+
+        // Make a bottom line
+        navRemoveBottomLine();
+        li.style.borderBottom = "5px solid #20de83";      
         break;
 
       case 'Menu':
         main.innerHTML = '';
         main.appendChild(loadMenu());
+
+        // Make a bottom line
+        navRemoveBottomLine();
+        li.style.borderBottom = "5px solid #20de83";      
         break;
 
       case 'Contact':
         main.innerHTML = '';
         main.appendChild(loadContact());
+
+        // Make a bottom line
+        navRemoveBottomLine();
+        li.style.borderBottom = "5px solid #20de83";      
         break;
     }    
   })
 }
 
+const navRemoveBottomLine = () => {
+  for(let i = 0; i < navBar.children.length; i++) {
+    navBar.children[i].style.borderBottom = '';
+  }
+}
 
 
 
